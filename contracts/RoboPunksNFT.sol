115b2798de33fecdc3d9705f30b59bbebebd0aad
Zhukovvlad <zhukovvlad@yandex.ru>
2022-06-06 16:15:18 +0300

    Initiated variables in contract

---
+++ contracts/RoboPunksNFT.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+contract RoboPunksNFT is ERC721, Ownable {
+    uint256 public mintPrice;
+    uint256 public totalSupply;
+    uint256 public maxSupply;
+    uint256 public maxPerWallet;
+    bool public isPublicMintEnabled;
+    string internal baseTokenUri;
+    address payable public withdrawWallet;
+    mapping(address => uint256) public walletMints;
+    
+}
