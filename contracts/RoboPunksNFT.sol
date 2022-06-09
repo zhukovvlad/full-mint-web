224d1c71f0b4dced295876825c75aab349ccb972
Zhukovvlad <zhukovvlad@yandex.ru>
2022-06-09 14:53:31 +0300

    Added mint NFT function

--- contracts/RoboPunksNFT.sol
+++ contracts/RoboPunksNFT.sol
@@ -40,4 +40,17 @@ contract RoboPunksNFT is ERC721, Ownable {
         require(success, 'withdraw failed');
     }
 
+    function mint(uint256 quantity_) public payable {
+        require(isPublicMintEnabled, 'minting not enabled');
+        require(msg.value == quantity_ * mintPrice, 'wrong mint value');
+        require(totalSupply + quantity_ <= maxSupply, 'sold out');
+        require(walletMints[msg.sender] + quantity_ <= maxPerWallet, 'exceed max wallet');
+
+        for (uint256 i = 0; i < quantity_; i++) {
+            uint256 newTokenId = totalSupply + 1;
+            totalSupply++;
+            _safeMint(msg.sender, newTokenId);
+        }
+    }
+
 }
